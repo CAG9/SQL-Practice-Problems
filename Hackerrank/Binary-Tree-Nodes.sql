fe9eb7805384181b180f2cba2127c3a9bde58e05
Cesar Arcos Gonzalez <60860385+CAG9@users.noreply.github.com>
2026-07-16 22:14:48 -0600

    Update Binary-Tree-Nodes.sql

--- Hackerrank/Binary-Tree-Nodes.sql
+++ Hackerrank/Binary-Tree-Nodes.sql
@@ -6,3 +6,14 @@ CASE
 END
     FROM BST A
     ORDER BY N;
+
+
+
+SELECT N,
+    CASE 
+     WHEN P IS NULL THEN "Root" 
+     WHEN N IN (SELECT P FROM BST) THEN "Inner"
+     ELSE "Leaf" 
+    END
+FROM BST
+ORDER BY N ASC 
