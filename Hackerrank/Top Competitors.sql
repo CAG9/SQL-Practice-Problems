1a370f772959b9bd6a0c3827a8edf3c2bcad3b35
Cesar Arcos Gonzalez <60860385+CAG9@users.noreply.github.com>
2026-07-17 10:50:40 -0600

    Update HAVING clause for completed challenges count
    Modified the HAVING clause to select hackers with more than one completed challenge instead of two.

--- Hackerrank/Top Competitors.sql
+++ Hackerrank/Top Competitors.sql
@@ -9,3 +9,25 @@ FROM submissions s
 GROUP BY h.hacker_id,h.name
 HAVING COUNT(s.hacker_id) > 1
 ORDER BY COUNT(s.hacker_id) DESC, h.hacker_id ASC;
+
+
+WITH completescore AS (
+    SELECT s.submission_id,
+           s.hacker_id,
+           s.challenge_id,
+           s.score AS attempt_score,
+           c.difficulty_level,
+           d.score AS total_points
+    FROM Submissions s
+    JOIN challenges c ON s.challenge_id = c.challenge_id
+    JOIN Difficulty d ON c.difficulty_level = d.difficulty_level AND s.score = d.score
+)
+
+SELECT h.hacker_id,
+       h.name
+FROM Hackers h
+JOIN completescore ON h.hacker_id = completescore.hacker_id
+GROUP BY h.hacker_id, h.name
+HAVING COUNT(completescore.challenge_id) > 1 -- Changed from > 2 to > 1
+ORDER BY COUNT(completescore.challenge_id) DESC, h.hacker_id ASC; -- Sorting stays on the main query
+
