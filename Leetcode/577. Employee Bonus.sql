3dd4247088e05fdad6f47eb2cfa5c5d01728659b
Cesar Arcos Gonzalez <60860385+CAG9@users.noreply.github.com>
2026-07-17 22:05:42 -0600

    Create 577. Employee Bonus.sql

---
+++ Leetcode/577. Employee Bonus.sql
@@ -0,0 +1,9 @@
+WITH complete  AS (
+SELECT e.empId,e.name,e.supervisor,e.salary, b.bonus
+FROM Employee e
+LEFT JOIN Bonus b
+ON e.empId = b.empid)
+
+SELECT name, bonus
+FROM complete
+WHERE bonus < 1000 OR bonus IS NULL
