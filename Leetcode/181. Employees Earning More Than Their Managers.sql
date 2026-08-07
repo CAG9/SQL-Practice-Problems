bec2c09203555f8affbcd745a7a9289389b340a5
Cesar Arcos Gonzalez <60860385+CAG9@users.noreply.github.com>
2026-08-07 11:34:28 -0600

    Update 181. Employees Earning More Than Their Managers.sql

--- Leetcode/181. Employees Earning More Than Their Managers.sql
+++ Leetcode/181. Employees Earning More Than Their Managers.sql
@@ -3,3 +3,10 @@ FROM employee employee1
 JOIN employee employee2
 ON employee1.managerId = employee2.id
 WHERE employee1.salary > employee2.salary
+
+
+--
+SELECT e1.name AS employee
+FROM employee e1
+JOIN employee e2
+ON e1.managerId = e2.id and e1.salary > e2.salary
